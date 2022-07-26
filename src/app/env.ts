2f466062ed59be9f62bf856cd577625a8ef222cd
Tajbiul Rawol <tajbiulrawol@gmail.com>
2022-07-27 02:12:58 +0600

    Update env.ts

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,6 +1,7 @@
 export class Environment {
    url = 'http://localhost:51727/API/TradeDetails/';
    pdfUrl = "http://localhost:51727/FileUpload/"
+   pdfUrl2 = "file:///D:/crudapp/API/NewAPI/FileUpload/"
    tradeLevelUrl = '../assets/store.json';
 }
 
